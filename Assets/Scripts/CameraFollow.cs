7a5561e1e1b111dcf37634779c4426f3da90090c
Nohbdy <Billen93@outlook.com>
2018-02-08 11:59:42 +0100

    Added project

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    float zoomSpeed = 1f;
+    [SerializeField]
+    float cameraDistance = 6;
+    [SerializeField]
+    float tmpPos;
+    [SerializeField] float smoothSpeed;
+    [SerializeField] float cameraDisMax = 10f;
+    [SerializeField] float cameraDisMin = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        cameraDistance = Camera.main.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.transform.position.x - transform.position.x >= 3 && target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position + new Vector3(target.position.x, -target.position.y, 0), 0.5f) + new Vector3(-3, 0, -10);
+        }
+        else if (target.transform.position.x - transform.position.x <= -3 && target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position + new Vector3(target.position.x , -target.position.y, 0), 0.5f) + new Vector3(3, 0, -10);
+
+        }
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, cameraDistance, smoothSpeed * Time.deltaTime);
+        //tmpCamPos = transform.position.x;
+        //tmpPlayPos = target.transform.position.x;
+        tmpPos = Mathf.Abs(target.transform.position.x - transform.position.x);
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 7;
+
+    [SerializeField]
+    int isJumping = 0;
+
+    Rigidbody rb;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(-6, 3, -2.0f);
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isJumping = 20;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (isJumping < 5)
+            {
+                gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 6.7f * speed, 0), 0);
+                //transform.position += ;
+                //gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                //gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                isJumping++;
+            }
+
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += move * speed * Time.deltaTime;
+
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "mapFloor")
+        {
+
+            isJumping = 0;
+        }
+    }
+}
